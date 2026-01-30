0152e7bfcb006bf5a2efa525c1e5ac63f9f0187d
parthi <parthiraji484@gmail.com>
2026-01-30 18:28:39 +0530

    myfirstcommit

---
+++ mytfproject/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_instance" "github_demo" {
+  ami           = "ami-039a8ebebdd2a1def" # Amazon Linux 2 (ap-southeast-1)
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "GitHubActions-Terraform-Server"
+  }
+}
---
+++ mytfproject/output.tf
@@ -0,0 +1,3 @@
+output "public_ip" {
+  value = aws_instance.github_demo.public_ip
+}
