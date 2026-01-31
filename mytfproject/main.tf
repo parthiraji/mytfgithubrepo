a3b51375d91403943ba38e65a4750b2c0a29717e
parthi <parthiraji484@gmail.com>
2026-01-31 12:03:58 +0530

    count 3 commit

--- mytfproject/main.tf
+++ mytfproject/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "github_demo" {
   ami           = "ami-039a8ebebdd2a1def" # Amazon Linux 2 (ap-southeast-1)
   instance_type = "t3.micro"
-  count = 2
+  count = 3
 
   tags = {
     Name = "GitHubActions-Terraform-Server"
